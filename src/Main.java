79ed672a35d084a4f47bba22c2c38e0da636f092
nbenin1 <nbenin@student.tgm.ac.at>
2023-10-04 16:41:09 +0200

    GUI, WortBildPaar verbunden/erstellt

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,38 @@ import javax.swing.*;
 import java.awt.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 public class Main {
     public static void main(String[] args) throws MalformedURLException {
-        JPanel panel = new JPanel(new BorderLayout());
-        ImageIcon bildWeg = new ImageIcon(new URL("https://www.kochschule.de/sites/default/files/images/kochwissen/440/banane.jpg"));
-        bildWeg.setImage(bildWeg.getImage().getScaledInstance(450,450, Image.SCALE_DEFAULT));
-        JLabel bild = new JLabel(bildWeg);
-        panel.add(bild, BorderLayout.PAGE_START);
+        while(true) {
+            JPanel panel = new JPanel(new BorderLayout());
+            WordBildPaar wp = new WordBildPaar(null);
+            String wortpaar = wp.randomPaar();
+            String[] teile = wortpaar.split(" - ");
 
-        JTextField text = new JTextField(10);
-        panel.add(text, BorderLayout.CENTER);
+            String wort = "";
+            String url = "";
+            if (teile.length == 2) {
+                wort = teile[0];
+                url = teile[1];
+            }
 
-        int jopion = JOptionPane.showConfirmDialog(null, panel, "Worttrainer Reloaded", JOptionPane.OK_CANCEL_OPTION);
+            ImageIcon bildWeg = new ImageIcon(new URL(url));
+            bildWeg.setImage(bildWeg.getImage().getScaledInstance(450, 450, Image.SCALE_DEFAULT));
+            JLabel bild = new JLabel(bildWeg);
+            panel.add(bild, BorderLayout.PAGE_START);
 
-        if (jopion == JOptionPane.OK_OPTION) {
-            String eingabe = text.getText();
-            JOptionPane.showMessageDialog(null, "Eingegebener Text: " + eingabe);
+            JTextField text = new JTextField(10);
+            panel.add(text, BorderLayout.CENTER);
+
+            int jopion = JOptionPane.showConfirmDialog(null, panel, "Worttrainer Reloaded", JOptionPane.OK_CANCEL_OPTION);
+
+            if (jopion == JOptionPane.OK_OPTION) {
+                String eingabe = text.getText();
+                JOptionPane.showMessageDialog(null, "Eingegebener Text: " + eingabe);
+            }
+            if (jopion == JOptionPane.CANCEL_OPTION) {
+                break;
+            }
         }
-
-        WordBildPaar wp = new WordBildPaar();
-
     }
 }
--- src/WordBildPaar.java
+++ src/WordBildPaar.java
@@ -5,32 +5,35 @@ import org.json.JSONTokener;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class WordBildPaar {
     private String url;
     private String wort;
-
-    WordBildPaar() {
+    private Map<Integer, String> map = new HashMap<>();
+    WordBildPaar(String pfad) {
         try {
-            JSONTokener tokener = new JSONTokener(new FileReader("D:\\Schule\\5.Klasse\\SEW\\Projekte\\ReloadedWorttrainer\\WordBilldPaar.json"));
-            JSONArray jsonArray = new JSONArray(tokener);
+            if(pfad == null){
+                map.put(1, "Banane" + " - " + "https://www.kochschule.de/sites/default/files/images/kochwissen/440/banane.jpg");
+                map.put(2, "Apfel" + " - " + "https://media.happycolorz.de/zeichnen-vorlagen/apfel-zeichnen-6.jpg");
+            }else {
+                JSONTokener tokener = new JSONTokener(new FileReader(pfad));
+                JSONArray jsonArray = new JSONArray(tokener);
 
-            Map<Integer, String> map = new HashMap<>();
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    int id = jsonObject.getInt("id");
+                    String wort = jsonObject.getString("wort");
+                    String url = jsonObject.getString("url");
 
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                int id = jsonObject.getInt("id");
-                String wort = jsonObject.getString("wort");
-                String url = jsonObject.getString("url");
-
-                map.put(id, wort + " - " + url);
+                    map.put(id, wort + " - " + url);
+                }
             }
             for (Map.Entry<Integer, String> entry : map.entrySet()) {
                 int id = entry.getKey();
                 String data = entry.getValue();
                 System.out.println("ID: " + id + ", Data: " + data);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -52,7 +55,22 @@ public class WordBildPaar {
         this.wort = wort;
     }
 
-    public void randomPaar(){
+    public String randomPaar(){
+        Random r = new Random();
+        int z = r.nextInt(map.size());
+        System.out.println(map.size());
+        System.out.println(z);
+        String s = map.get(z);
+        System.out.println(s);
+        return s;
+    }
 
+    public String fixPaar(int id){
+        String s = map.get(id);
+        System.out.println(s);
+        return s;
+    }
+    public Map<Integer,String> getMap(){
+        return map;
     }
 }
